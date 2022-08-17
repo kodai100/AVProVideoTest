273240dd3bf5e45f364a3842d2f720898fd07f34
Kodai Takao (Fork HomeWorkstation) <kodai100@gmail.com>
2022-08-17 23:22:06 +0900

    mode change test

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,18 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using RenderHeads.Media.AVProDeckLink;
 using UnityEngine;
 
 public class Test : MonoBehaviour
 {
 
+    [SerializeField] private int mode;
+
+    [SerializeField] private IMGUIDisplay display;
+    
+    private DeckLinkInput a;
+
     // Start is called before the first frame update
     private void Start()
     {
 
-        var a = gameObject.AddComponent<DeckLinkInput>();
+        a = gameObject.AddComponent<DeckLinkInput>();
+
+        display._inputDecklink = a;
+
+    }
+
+    private int prevMode;
+    
+    private void Update()
+    {
+
+        mode = Mathf.FloorToInt(Mathf.Max((float)mode, -1f));
         
-        a.DeviceIndex = 0;
+        if (prevMode != mode)
+        {
+            a.ModeIndex = mode;
+            a.Begin();
+        }
+
+        prevMode = mode;
 
     }
 
